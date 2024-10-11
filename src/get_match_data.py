86b653296f8f724c7f2ed627058e741d25594f4a
free-the-malloc <cooper.gee@hotmail.com>
2024-10-11 12:35:10 +1100

    Changes to src files
    Modified the page timeout response mechanism for both get round and
    match functions. Previously it was 45 seconds and exited after a single
    attempt. That has been changed to 10 seconds, and three attempts are
    made before failure is accepted.

--- src/get_match_data.py
+++ src/get_match_data.py
@@ -10,89 +10,99 @@ def get_match_data(extension,attributes):
     options.add_argument('--ignore-certificate-errors')
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
     options.add_argument('--headless')
+    options.setPageLoadTimeout(10)
     options.add_experimental_option(
         "prefs", {
             "profile.managed_default_content_settings.images": 2,
         }
     )
     driver = webdriver.Chrome(options=options)
-    driver.set_page_load_timeout(45)
-    try:
-        driver.get(url)
-    except:
+    
+    # Allow a maximum of 3 page requests before throwing an error
+    for i in range(3):
+        try:
+            driver.get(url)
+        except:
+            driver.quit()
+            continue
+
+        page_source = driver.page_source
         driver.quit()
-        return None
-
-    page_source = driver.page_source
-    driver.quit()
+        
+        time.sleep(1)
     
-    time.sleep(6)
- 
-    # Parse the webpage source into a BeautifulSoup object
-    soup = BeautifulSoup(page_source, "html.parser")
+        # Parse the webpage source into a BeautifulSoup object
+        soup = BeautifulSoup(page_source, "html.parser")
 
-    # Get the team stats tile
-    team_stats_box = soup.find_all("div",class_="u-spacing-pb-24 u-spacing-pt-16 u-width-100")
+        # Get the team stats tile
+        team_stats_box = soup.find_all("div",class_="u-spacing-pb-24 u-spacing-pt-16 u-width-100")
 
-    # Actual data we're looking for will be in an element under these classes
-    home_stats_class = ["stats-bar-chart__label stats-bar-chart__label--home",
-                        "stats-bar-chart__label stats-bar-chart__label--home u-font-weight-700"]
-    away_stats_class = ["stats-bar-chart__label stats-bar-chart__label--away",
-                        "stats-bar-chart__label stats-bar-chart__label--away u-font-weight-700"]
-    
-    home_possession_class = ["match-centre-card-donut__value match-centre-card-donut__value--home",
-                             "match-centre-card-donut__value match-centre-card-donut__value--home u-font-weight-500"]
-    away_possession_class = ["match-centre-card-donut__value match-centre-card-donut__value--away u-font-weight-500",
-                             "match-centre-card-donut__value match-centre-card-donut__value--away"]
+        # Actual data we're looking for will be in an element under these classes
+        home_stats_class = ["stats-bar-chart__label stats-bar-chart__label--home",
+                            "stats-bar-chart__label stats-bar-chart__label--home u-font-weight-700"]
+        away_stats_class = ["stats-bar-chart__label stats-bar-chart__label--away",
+                            "stats-bar-chart__label stats-bar-chart__label--away u-font-weight-700"]
+        
+        home_possession_class = ["match-centre-card-donut__value match-centre-card-donut__value--home",
+                                "match-centre-card-donut__value match-centre-card-donut__value--home u-font-weight-500"]
+        away_possession_class = ["match-centre-card-donut__value match-centre-card-donut__value--away u-font-weight-500",
+                                "match-centre-card-donut__value match-centre-card-donut__value--away"]
 
-    completion_class = ["match-centre-card-donut__value match-centre-card-donut__value--footer u-font-weight-500",
-                        "match-centre-card-donut__value match-centre-card-donut__value--footer"]
+        completion_class = ["match-centre-card-donut__value match-centre-card-donut__value--footer u-font-weight-500",
+                            "match-centre-card-donut__value match-centre-card-donut__value--footer"]
 
-    play_the_ball_class = "donut-chart-stat__value"
-    
-    match_data = {}
+        play_the_ball_class = "donut-chart-stat__value"
+        
+        match_data = {}
 
-    for stats_box_i in team_stats_box:
-        stat_box_label = stats_box_i.find("h3")
-        if stat_box_label != None:
-            stat_box_label = stat_box_label.text
-            
-            if stat_box_label in attributes:
-                if stat_box_label == "Completion Rate":
-                    completion_rate = stats_box_i.find_all("p",{"class":completion_class})
+        for stats_box_i in team_stats_box:
+            stat_box_label = stats_box_i.find("h3")
+            if stat_box_label != None:
+                stat_box_label = stat_box_label.text
+                
+                if stat_box_label in attributes:
+                    if stat_box_label == "Completion Rate":
+                        completion_rate = stats_box_i.find_all("p",{"class":completion_class})
+                        
+                        home_completion_rate = completion_rate[0].text.strip().split("/")
+                        match_data["Home Completed Sets"] = int(home_completion_rate[0])
+                        match_data["Home Total Sets"] = int(home_completion_rate[1])
+
+                        away_completion_rate = completion_rate[1].text.strip().split("/")
+                        match_data["Away Completed Sets"] = int(away_completion_rate[0])
+                        match_data["Away Total Sets"] = int(away_completion_rate[1])
                     
-                    home_completion_rate = completion_rate[0].text.strip().split("/")
-                    match_data["Home Completed Sets"] = int(home_completion_rate[0])
-                    match_data["Home Total Sets"] = int(home_completion_rate[1])
+                    if stat_box_label == "Average Play The Ball Speed":
+                        play_the_ball_speeds = stats_box_i.find_all("p",{"class":play_the_ball_class})
+                        match_data[f"Home {stat_box_label}"] = float(play_the_ball_speeds[0].text.strip("s"))
+                        match_data[f"Away {stat_box_label}"] = float(play_the_ball_speeds[1].text.strip("s"))
+                    
+                    if stat_box_label == "Kick Defusal %":
+                        kick_defusal = stats_box_i.find_all("p",{"class":"donut-chart-stat__value"})
+                        match_data[f"Home {stat_box_label}"] = int(kick_defusal[0].text.strip("%"))
+                        match_data[f"Away {stat_box_label}"] = int(kick_defusal[1].text.strip("%"))
+                    
+                    if stat_box_label == "Possession %":
+                        match_data[f"Home {stat_box_label}"] = int(stats_box_i.find("p",{"class":home_possession_class}).text.strip("%"))
+                        match_data[f"Away {stat_box_label}"] = int(stats_box_i.find("p",{"class":away_possession_class}).text.strip("%"))
 
-                    away_completion_rate = completion_rate[1].text.strip().split("/")
-                    match_data["Away Completed Sets"] = int(away_completion_rate[0])
-                    match_data["Away Total Sets"] = int(away_completion_rate[1])
-                
-                if stat_box_label == "Average Play The Ball Speed":
-                    play_the_ball_speeds = stats_box_i.find_all("p",{"class":play_the_ball_class})
-                    match_data[f"Home {stat_box_label}"] = float(play_the_ball_speeds[0].text.strip("s"))
-                    match_data[f"Away {stat_box_label}"] = float(play_the_ball_speeds[1].text.strip("s"))
-                
-                if stat_box_label == "Kick Defusal %":
-                    kick_defusal = stats_box_i.find_all("p",{"class":"donut-chart-stat__value"})
-                    match_data[f"Home {stat_box_label}"] = int(kick_defusal[0].text.strip("%"))
-                    match_data[f"Away {stat_box_label}"] = int(kick_defusal[1].text.strip("%"))
-                
-                if stat_box_label == "Possession %":
-                    match_data[f"Home {stat_box_label}"] = int(stats_box_i.find("p",{"class":home_possession_class}).text.strip("%"))
-                    match_data[f"Away {stat_box_label}"] = int(stats_box_i.find("p",{"class":away_possession_class}).text.strip("%"))
+            stat_box_label = stats_box_i.find("figcaption").text        
+            if stat_box_label in attributes:
+                if stat_box_label == "Average Set Distance":
+                    match_data[f"Home {stat_box_label}"] = float(stats_box_i.find("dd",{"class":home_stats_class}).text.strip())
+                    match_data[f"Away {stat_box_label}"] = float(stats_box_i.find("dd",{"class":away_stats_class}).text.strip())
+                elif stat_box_label == "Time In Possession":
+                    home_time = stats_box_i.find("dd",{"class":home_stats_class}).text.strip().split(":")
+                    match_data[f"Home {stat_box_label}"] = int(home_time[0]) + (int(home_time[1]) / 60)
+                    
+                    away_time = stats_box_i.find("dd",{"class":away_stats_class}).text.strip().split(":")
+                    match_data[f"Away {stat_box_label}"] = int(away_time[0]) + (int(away_time[1]) / 60)
+                else:
+                    match_data[f"Home {stat_box_label}"] = int(stats_box_i.find("dd",{"class":home_stats_class}).text.strip(","))
+                    match_data[f"Away {stat_box_label}"] = int(stats_box_i.find("dd",{"class":away_stats_class}).text.strip(","))
 
-        stat_box_label = stats_box_i.find("figcaption").text        
-        if stat_box_label in attributes:
-            if stat_box_label == "Average Set Distance":
-                match_data[f"Home {stat_box_label}"] = float(stats_box_i.find("dd",{"class":home_stats_class}).text.strip())
-                match_data[f"Away {stat_box_label}"] = float(stats_box_i.find("dd",{"class":away_stats_class}).text.strip())
-            elif stat_box_label == "Time In Possession":
-                match_data[f"Home {stat_box_label}"] = stats_box_i.find("dd",{"class":home_stats_class}).text.strip()
-                match_data[f"Away {stat_box_label}"] = stats_box_i.find("dd",{"class":away_stats_class}).text.strip()
-            else:
-                match_data[f"Home {stat_box_label}"] = int(stats_box_i.find("dd",{"class":home_stats_class}).text.strip(","))
-                match_data[f"Away {stat_box_label}"] = int(stats_box_i.find("dd",{"class":away_stats_class}).text.strip(","))
-
-    return match_data
+        # Match data read was successful
+        return match_data
+    
+    # Page timed out 3 times
+    return None
--- src/get_round_data.py
+++ src/get_round_data.py
@@ -17,59 +17,62 @@ def get_round_data(round,year,attributes):
         }
     )
     options.add_argument("--headless")
+    options.setPageLoadTimeout(10)
     driver = webdriver.Chrome(options=options)
-    driver.set_page_load_timeout(45)
-    try:
-        driver.get(url)
-    except:
-        driver.quit()
-        return None
-   
-    page_source = driver.page_source
-    driver.quit()
-
-    time.sleep(6)
-
-    soup = BeautifulSoup(page_source, "html.parser")
+    for i in range(3):
+        try:
+            driver.get(url)
+        except:
+            driver.quit()
+            continue
     
-    # Get each match tile
-    matches = soup.find_all("div", class_="match o-rounded-box o-shadowed-box")
+        page_source = driver.page_source
+        driver.quit()
 
-    # Array to hold the stats from each match in the round
-    round_data = []
+        time.sleep(2)
 
-    for match in matches:
-        # Get the extension for the match
-        match_extension = match.find("a", class_="match--highlighted u-flex-column u-flex-align-items-center u-width-100")
+        soup = BeautifulSoup(page_source, "html.parser")
+        
+        # Get each match tile
+        matches = soup.find_all("div", class_="match o-rounded-box o-shadowed-box")
 
-        match_general = {}
-        
-        attribute_tag = ["h3", "p", 
-                         "p", "div", 
-                         "p", "div"]
-        
-        attribute_class = ["u-visually-hidden", "match-header__title", 
-                           "match-team__name--home", "match-team__score--home", 
-                           "match-team__name--away", "match-team__score--away"]
-        
-        attribute_name = ["Details","Date",
-                          "Home","Home Score",
-                          "Away","Away Score"]
-        
-        match_general["Round"] = round
+        # Array to hold the stats from each match in the round
+        round_data = []
 
-        for i in range(0,6):
-            match_general[attribute_name[i]] = match.find(attribute_tag[i],class_=attribute_class[i]).text.strip()
-        
-        match_general["Home Score"] = int(match_general["Home Score"].replace("Scored","").replace("points","").strip())
-        match_general["Away Score"] = int(match_general["Away Score"].replace("Scored","").replace("points","").strip())
+        for match in matches:
+            # Get the extension for the match
+            match_extension = match.find("a", class_="match--highlighted u-flex-column u-flex-align-items-center u-width-100")
 
-        # Get the statistics for the match
-        match_stats = get_match_data(match_extension["href"],attributes)
-        
-        # If the stats from a round couldn't be scraped, return none
-        if match_stats == None:
-            return None
-        # Otherwise append the match stats to the array
-        round_data.append({**match_general,**match_stats})
-    return round_data
+            match_general = {}
+            
+            attribute_tag = ["h3", "p", 
+                            "p", "div", 
+                            "p", "div"]
+            
+            attribute_class = ["u-visually-hidden", "match-header__title", 
+                            "match-team__name--home", "match-team__score--home", 
+                            "match-team__name--away", "match-team__score--away"]
+            
+            attribute_name = ["Details","Date",
+                            "Home","Home Score",
+                            "Away","Away Score"]
+            
+            match_general["Round"] = round
+
+            for i in range(0,6):
+                match_general[attribute_name[i]] = match.find(attribute_tag[i],class_=attribute_class[i]).text.strip()
+            
+            match_general["Home Score"] = int(match_general["Home Score"].replace("Scored","").replace("points","").strip())
+            match_general["Away Score"] = int(match_general["Away Score"].replace("Scored","").replace("points","").strip())
+
+            # Get the statistics for the match
+            match_stats = get_match_data(match_extension["href"],attributes)
+            
+            # If the stats from a round couldn't be scraped, return none
+            if match_stats == None:
+                return None
+            # Otherwise append the match stats to the array
+            round_data.append({**match_general,**match_stats})
+        return round_data
+    
+    return None
